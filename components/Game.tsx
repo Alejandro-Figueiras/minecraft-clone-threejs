f5d3a9b781b467b6d736959921824da5294136bc
Alejandro <alejandrofigueirasx@gmail.com>
2025-06-24 21:25:05 -0400

    Add grass texture

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,21 +1,21 @@
-import { Physics } from "@react-three/cannon"
-import { Sky } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import React from "react"
-import Ground from "./Ground"
-import FirstPointView from "./FirstPointView"
+import { Physics } from '@react-three/cannon'
+import { Sky } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import Ground from './Ground'
+import FirstPointView from './FirstPointView'
 
 const Game = () => {
-	return (
-		<Canvas className='h-dvh w-full'>
-			<Sky />
-			<ambientLight intensity={0.5} />
-			<FirstPointView />
-			<Physics>
-				<Ground />
-			</Physics>
-		</Canvas>
-	)
+  return (
+    <Canvas className='h-dvh w-full'>
+      <Sky />
+      <ambientLight intensity={0.8} />
+      <FirstPointView />
+      <Physics>
+        <Ground />
+      </Physics>
+    </Canvas>
+  )
 }
 
 export default Game
--- components/Ground.tsx
+++ components/Ground.tsx
@@ -1,18 +1,29 @@
-import { usePlane } from "@react-three/cannon"
-import React from "react"
+'use client'
+import { groundImg } from '@/textures/textures'
+import { usePlane } from '@react-three/cannon'
+import { useTexture } from '@react-three/drei'
+import React from 'react'
+import { NearestFilter, RepeatWrapping } from 'three'
 
 const Ground = () => {
-	const [ref] = usePlane(() => ({
-		rotation: [-Math.PI / 2, 0, 0],
-		position: [0, -0.5, 0],
-	}))
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -0.5, 0]
+  }))
 
-	return (
-		<mesh ref={ref}>
-			<planeGeometry attach='geometry' args={[100, 100]} />
-			<meshStandardMaterial attach='material' color='green' />
-		</mesh>
-	)
+  const groundTexture = useTexture(groundImg)
+
+  groundTexture.wrapS = RepeatWrapping
+  groundTexture.wrapT = RepeatWrapping
+  groundTexture.repeat.set(100, 100)
+  groundTexture.magFilter = NearestFilter
+
+  return (
+    <mesh ref={ref}>
+      <planeGeometry attach='geometry' args={[100, 100]} />
+      <meshStandardMaterial attach='material' map={groundTexture} />
+    </mesh>
+  )
 }
 
 export default Ground
